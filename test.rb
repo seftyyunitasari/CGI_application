9f647f5ceb5c87d322daee7d3ab3189d56d88660
sefty <seftyyunitasari@gmail.com>
2021-11-12 15:35:47 +0900

    set assignment page as a deafult page

--- test.rb
+++ test.rb
@@ -13,7 +13,7 @@ server = WEBrick::HTTPServer.new({
 server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
-server.mount('/assignment', WEBrick::HTTPServlet::ERBHandler, 'assignment.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'assignment.html.erb')
 server.mount('/assignment1.cgi', WEBrick::HTTPServlet::CGIHandler, 'assignment1.rb')
 server.mount('/assignment2.cgi', WEBrick::HTTPServlet::CGIHandler, 'assignment2.rb')
 
